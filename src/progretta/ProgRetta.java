47450fa98748f3c418f58f2938361b3fe7134c25
william.parola <william.parola@InfoTele21.maxwell.local>
2022-12-12 11:05:13 +0100

    ctyytgtgf

---
+++ src/progretta/ProgRetta.java
@@ -0,0 +1,14 @@
+
+package progretta;
+
+
+public class ProgRetta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
